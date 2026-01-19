fec2340fe2f510a266c3d5e0d762fc5925ddaebb
arthurwillers <arthurwillers@proton.me>
2026-01-19 09:49:39 -0300

    Update font family in app.css and add head.blade.php for HTML structure

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>{{ config('app.name') }}</title>
+
+<!-- favicons -->
+<link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+<!-- Styles / Scripts -->
+@vite(['resources/css/app.css', 'resources/js/app.js'])
